cfb73c7317e9472e3541ff4be735a1b654abea1e
devcrack <jaha.devcrack@gmail.com>
2017-05-21 23:26:00 -0500

    Define a\ Terminal Symbol.

---
+++ src/symbol/C_T.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package symbol;
+
+/**
+ *
+ * @author devcrack
+ */
+public class C_T extends C_Atomic_Symbol {
+
+    public C_T(){ super(); } 
+    
+    /**
+     * Gets the terminal 
+     * @return 
+     */
+    public String get_T() {
+        return super.get_Atomic_Symbol();
+    }
+}
