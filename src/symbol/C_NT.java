8013afb37595aa7ce3217dd8fec04d612af91237
devcrack <jaha.devcrack@gmail.com>
2017-05-21 23:24:47 -0500

    Define a Non Terminal Symbol.

---
+++ src/symbol/C_NT.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package symbol;
+
+/**
+ *
+ * @author devcrack
+ */
+public class C_NT extends C_Atomic_Symbol{
+ 
+    public C_NT(){ 
+        super();
+    } 
+    
+    public String get_NT() {
+        return super.get_Atomic_Symbol();
+    }
+}
