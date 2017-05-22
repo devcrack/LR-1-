497d78dd80be5989296630575ef533a8b3e6a997
devcrack <jaha.devcrack@gmail.com>
2017-05-21 23:23:36 -0500

    Is the simplest way to represent a symbol for a production.

---
+++ src/symbol/C_Atomic_Symbol.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package symbol;
+
+/**
+ *
+ * @author devcrack
+ */
+public class C_Atomic_Symbol {
+    
+    protected String symbol;
+    
+    public C_Atomic_Symbol()
+    {
+        this.symbol = new String();
+    }
+
+    /**
+     * @return the simbolos
+     */
+    public String get_Atomic_Symbol() {
+        return symbol;
+    }
+
+    /**
+     * @param simbolos the simbolos to set
+     */
+    public void set_Atomic_Symbol(String symbol) {
+        this.symbol = symbol;
+    }
+    
+    protected boolean is_META_character()
+    {
+        String metacharacters =  " + * < > | \\\\";
+        
+        if(metacharacters.contains(this.symbol))
+             return true;
+        else 
+            return false;
+    }
+}
