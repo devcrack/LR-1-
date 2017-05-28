0df5699d686c364c9c38e518999453719648469a
devcrack <jaha.devcrack@gmail.com>
2017-05-28 11:58:34 -0500

    Append the searcher symbol to the productions

--- src/lr/pkg1/lr_1.java
+++ src/lr/pkg1/lr_1.java
@@ -6,6 +6,9 @@
 package lr.pkg1;
 
 import grammar_things.C_Grammar;
+import grammar_things.C_Production;
+import java.util.Iterator;
+import symbol.C_Symbol;
 
 /**
  *
@@ -14,7 +17,24 @@ import grammar_things.C_Grammar;
 public class lr_1 {
     C_Grammar increased_grammar;
     
-    public lr_1() {
-        
+    public lr_1(C_Grammar gram) {
+        this.increased_grammar = new C_Grammar(gram);
+        this.make_extended();
     }
+    
+    /**
+     * Append the searcher symbol
+     */
+    private void make_extended() {
+        Iterator it  = this.increased_grammar.getGrammar().iterator();
+        C_Production tmp_pr;
+        C_Symbol searcher_symbol; 
+        
+        while(it.hasNext()) {
+           tmp_pr = (C_Production)it.next();
+           searcher_symbol = new C_Symbol(".");
+           tmp_pr.getRight().add(0, searcher_symbol);
+        }
+    }
+    
 }
