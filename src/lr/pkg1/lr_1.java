1eba47e65e7b9b497f04bdf66345a01a792ed5c8
devcrack <jaha.devcrack@gmail.com>
2017-05-27 22:14:50 -0500

    Start with FDA,before play LoL

---
+++ src/lr/pkg1/lr_1.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lr.pkg1;
+
+import grammar_things.C_Grammar;
+
+/**
+ *
+ * @author devcrack
+ */
+public class lr_1 {
+    C_Grammar increased_grammar;
+    
+    public lr_1() {
+        
+    }
+}
