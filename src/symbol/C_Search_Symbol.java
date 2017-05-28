25a3e016e47477c17ee84967f7dc5d2619937fc5
devcrack <jaha.devcrack@gmail.com>
2017-05-28 11:52:30 -0500

    constructor update

--- src/symbol/C_Search_Symbol.java
+++ src/symbol/C_Search_Symbol.java
@@ -15,6 +15,10 @@ public class C_Search_Symbol extends C_Atomic_Symbol{
         super();
     }
     
+    public C_Search_Symbol(String srch_smbl) {
+        super(srch_smbl);
+    }
+    
     public String get_Search_Symbol(){return super.get_Atomic_Symbol();}
    
 }
