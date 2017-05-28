0053f37c92c1ef8e800cb259c7d0ef818d973fe3
devcrack <jaha.devcrack@gmail.com>
2017-05-28 10:14:00 -0500

    Class that management the Search Symbol

---
+++ src/symbol/C_Search_Symbol.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package symbol;
+
+/**
+ *
+ * @author devcrack
+ */
+public class C_Search_Symbol extends C_Atomic_Symbol{
+    
+    public C_Search_Symbol() {
+        super();
+    }
+    
+    public String get_Search_Symbol(){return super.get_Atomic_Symbol();}
+   
+}
