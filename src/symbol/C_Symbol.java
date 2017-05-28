f479cdbc75ab04d799a88f2c80921837777bc2ba
devcrack <jaha.devcrack@gmail.com>
2017-05-28 10:13:41 -0500

    Update for accept the Search Symbol

--- src/symbol/C_Symbol.java
+++ src/symbol/C_Symbol.java
@@ -10,13 +10,13 @@ package symbol;
  * @author devcrack
  */
 public class C_Symbol {
-    
-     private C_NT nt;
-    private C_T   t;
+
+    private C_NT nt;
+    private C_T t;
     private C_Epsilon epsilon;
-    
-    public C_Symbol()
-    {
+    private C_Search_Symbol srch_Symbol;
+
+    public C_Symbol() {
         this.nt = new C_NT();
         this.t = new C_T();
         this.epsilon = new C_Epsilon();
@@ -35,23 +35,22 @@ public class C_Symbol {
     public String getT() {
         return t.get_T();
     }
-    
-    
+
     /**
      * Set the corresponding String to Terminal Symbol
-     * @param str_T 
+     *
+     * @param str_T
      */
-    public void set_T(String str_T)
-    {
+    public void set_T(String str_T) {
         this.t.set_Atomic_Symbol(str_T);
     }
-    
+
     /**
      * Set the correponding string to No Terminal Symbol
+     *
      * @param str_NT Given String for set to the No terminal Symbol
      */
-    public void set_NT(String str_NT)
-    {
+    public void set_NT(String str_NT) {
         this.nt.set_Atomic_Symbol(str_NT);
     }
 
@@ -68,16 +67,41 @@ public class C_Symbol {
     public void setEpsilon(String s_epsilon) {
         this.epsilon.set_Atomic_Symbol(s_epsilon);
     }
+
     /**
      * Determine if the Non Terminal Symbol is Empty;
+     *
      * @return True if the Non Terminal Symbols is Empty;
      */
-    public boolean is_NT_EMPTY() { return (this.nt.get_NT().length()  == 0) ? true : false; }
+    public boolean is_NT_EMPTY() {
+        return (this.nt.get_NT().length() == 0) ? true : false;
+    }
+
     /**
      * Determine if the Terminal Symbols is empty;
+     *
      * @return True if the TERMINAL symbols do not have anything.
      */
-    public boolean is_T_EMPTY() { return (this.t.get_T().length()  == 0) ? true : false; }    
-    public boolean TERMINAL_is_Metacharecter() { return this.t.is_META_character();}
-    
+    public boolean is_T_EMPTY() {
+        return (this.t.get_T().length() == 0) ? true : false;
+    }
+
+    public boolean TERMINAL_is_Metacharecter() {
+        return this.t.is_META_character();
+    }
+
+    /**
+     * @return the srch_Symbol
+     */
+    public C_Search_Symbol getSrch_Symbol() {
+        return srch_Symbol;
+    }
+
+    /**
+     * @param srch_Symbol the srch_Symbol to set
+     */
+    public void setSrch_Symbol(String srch_smbl) {
+        this.srch_Symbol.set_Atomic_Symbol(srch_smbl);
+    }
+
 }
